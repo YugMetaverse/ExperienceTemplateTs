20100fcd740bc84e35195d224925f24807fb85e0
Utkarsh Shukla <utk.shukla@gmail.com>
2023-05-01 14:42:08 +0530

    Template Repository

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
 /*
 @YugObject
-id: 123123123123123,
-itemDisplayName: UI Script,
+id: 123456789,
+itemDisplayName: Example Script,
 itemType: Script,
-itemPath: scripts/index.js,
+itemPath: js/index.js,
 baseActorToSpawn: ScriptActor,
 itemDefaultProperties: []
 */
 
-import { Hello } from "./dep";
+
 import * as puerts from "puerts";
 import * as UE from  "ue";
-import * as UI from './main-ui';
 import {ReactUMG} from "react-umg";
 const world : UE.World = globalThis.World;
 
-console.log("Current Platform: " + UE.GameplayStatics.GetPlatformName());
 console.log("Hello World");
 
-const hello = new Hello();
-hello.description;
-hello.TestFunction();
-ReactUMG.init(world);
-UI.Load();
